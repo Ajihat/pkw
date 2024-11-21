e4709c3a75018a8531669f338cc7c098f4102f35
Dawid Lechowski <david.lechowski@gmail.com>
2024-11-21 20:14:21 +0100

    next

--- src/app/(data)/portfolioData.ts
+++ src/app/(data)/portfolioData.ts
@@ -3,14 +3,14 @@ export const portfolioData = [
 		id: 1,
 		imageSrc: './portfolio/portfolio1.png',
 		date: 'August 27, 2020',
-		header: 'Why We Decided To Create Build Your Dxp',
+		header: 'Why We Decided',
 		content: 'asasd',
 	},
 	{
 		id: 2,
 		imageSrc: './portfolio/portfolio2.png',
 		date: 'August 27, 2020',
-		header: 'Why We Decided To Create Build Your Dxp',
+		header: 'Why We Decided To Create Build Your Dxp Create Build Your Dxp',
 		content: 'asasd',
 	},
 	{
