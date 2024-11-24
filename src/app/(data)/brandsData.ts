b0c3dfb4f740ac3c65b6c2a7a4aebdaef03172ec
Dawid Lechowski <david.lechowski@gmail.com>
2024-11-24 18:47:41 +0100

    next

--- src/app/(data)/brandsData.ts
+++ src/app/(data)/brandsData.ts
@@ -2,33 +2,41 @@ export const brandsData = [
 	{
 		id: 1,
 		imageSrc: './brands/brand1.png',
+		alt: 'Nazwa firmy',
 	},
 	{
 		id: 2,
 		imageSrc: './brands/brand2.png',
+		alt: 'Nazwa firmy',
 	},
 	{
 		id: 3,
 		imageSrc: './brands/brand3.png',
+		alt: 'Nazwa firmy',
 	},
 	{
 		id: 4,
 		imageSrc: './brands/brand4.png',
+		alt: 'Nazwa firmy',
 	},
 	{
 		id: 5,
 		imageSrc: './brands/brand1.png',
+		alt: 'Nazwa firmy',
 	},
 	{
 		id: 6,
 		imageSrc: './brands/brand2.png',
+		alt: 'Nazwa firmy',
 	},
 	{
 		id: 7,
 		imageSrc: './brands/brand3.png',
+		alt: 'Nazwa firmy',
 	},
 	{
 		id: 8,
 		imageSrc: './brands/brand4.png',
+		alt: 'Nazwa firmy',
 	},
 ];
